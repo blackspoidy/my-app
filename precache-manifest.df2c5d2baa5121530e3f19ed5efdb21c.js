64633e83a9f8b1af1ef930ede05876914f95d2cb
Black Spoidy <khanroshaan9@gmail.com>
2022-10-19 19:17:04 +0500

    Updates

---
+++ precache-manifest.df2c5d2baa5121530e3f19ed5efdb21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4b7767103e1e124aa5",
+    "url": "/my-app/static/css/main.6f61f75a.chunk.css"
+  },
+  {
+    "revision": "da4b7767103e1e124aa5",
+    "url": "/my-app/static/js/main.da4b7767.chunk.js"
+  },
+  {
+    "revision": "82d436f42c4be3b1e980",
+    "url": "/my-app/static/js/1.82d436f4.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/my-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "f4cbf9559e808e99bf06",
+    "url": "/my-app/static/js/runtime~main.f4cbf955.js"
+  },
+  {
+    "revision": "22b3d666f4b856890b24614091ca3886",
+    "url": "/my-app/index.html"
+  }
+];
